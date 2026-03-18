207b8ae3f9b3638e9488c9e7a05fbc6071879d58
loubnaamez10 <lina.loubnaa@gmail.com>
2026-03-18 12:26:34 +0100

    done

--- Plateforme/resources/migrations/0010_drop_corpus_size_and_file_format.py
+++ Plateforme/resources/migrations/0010_drop_corpus_size_and_file_format.py
@@ -3,7 +3,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("resources", "0009_corpus_rejection_reason_course_rejection_reason_and_more"),
+        ("resources", "0008_corpus_rejection_reason_course_rejection_reason_and_more"),
     ]
 
     # Fields were already removed in earlier migration history on this branch.
