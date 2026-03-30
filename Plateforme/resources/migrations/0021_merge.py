69da25c57a866a6bce847058baba078b55a61c3e
loubnaamez10 <lina.loubnaa@gmail.com>
2026-03-30 22:05:45 +0100

    Add comprehensive merge migrations to reconcile all branches

---
+++ Plateforme/resources/migrations/0021_merge.py
@@ -0,0 +1,14 @@
+# Generated manually to reconcile all divergent resources migration branches
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("resources", "0012_nlptool_thumbnail"),
+        ("resources", "0013_drop_legacy_corpus_file_format_column"),
+        ("resources", "0020_merge_20260330_1105"),
+    ]
+
+    operations = []
