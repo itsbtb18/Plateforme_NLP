eab765b5e9c507144dc75b8264ad8ff0c7a1c3a1
loubnaamez10 <lina.loubnaa@gmail.com>
2026-03-31 20:36:09 +0100

    Corpus details page

--- Plateforme/resources/migrations/0020_merge_20260330_1105.py
+++ Plateforme/resources/migrations/0020_merge_20260330_1105.py
@@ -6,12 +6,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-<<<<<<< HEAD
-=======
-        ("resources", "0012_nlptool_thumbnail"),
-        ("resources", "0013_drop_legacy_corpus_file_format_column"),
-        ("resources", "0019_corpus_rejection_reason_course_rejection_reason_and_more"),
->>>>>>> 779b67ef092712b4e38de6343e93e2464836229a
         ("resources", "0019_corpus_rejection_reason_course_rejection_reason_and_more"),
         ("resources", "0019_resource_entities"),
         ("resources", "0019_resource_soft_delete_fields"),
